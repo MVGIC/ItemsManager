55e654bed623d281fd047d8216e4232bd64a0288
Николай <ex3ct@mail.ru>
2020-12-10 15:28:09 +0700

    added getters and setters

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -7,4 +7,12 @@ public class Smartphone extends Product {
         super(id, name, cost);
         this.producer = producer;
     }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
 }
